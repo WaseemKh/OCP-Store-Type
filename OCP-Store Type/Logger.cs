8678ae1924e7fae6847ddb1b9ba982f84624f902
Waseem Khalid <waseemsabra2@gmail.com>
2022-11-04 15:42:52 +0200

    Here we use another eample with polymorphism by virtual and inhertance override methode to  achivement the -OCP

---
+++ OCP-Store Type/Logger.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP_Store_Type
+{
+    
+
+        public class MainLogger
+        {
+            public virtual void Log(string message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public virtual void Info(string message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public virtual void Debug(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+
+
+
+
+    public class DevLogger:MainLogger
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public override void Info(string message)
+        {
+            Console.WriteLine($"Info: {message}");
+        }
+
+        public override void Debug(string message)
+        {
+            Console.WriteLine($"Debug: {message}");
+        }
+    }
+
+
+    public class ReleaseLogger:MainLogger 
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine($"Please ... :{message}");
+        }
+
+        public override void Info(string message)
+        {
+            Console.WriteLine($"This is : {message}");
+        }
+
+        public override void Debug(string message)
+        {
+            Console.WriteLine($"Debug: {message}");
+        }
+    }
+
+
+}
