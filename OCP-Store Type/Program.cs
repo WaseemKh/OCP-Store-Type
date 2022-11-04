7fa810e5f1b165bdac65361ce188216518817670
Waseem Khalid <waseemsabra2@gmail.com>
2022-11-04 15:21:22 +0200

    Add project files.

---
+++ OCP-Store Type/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Store
+{
+    public int StoreId { get; set; }
+    public string StoreName { get; set; }
+    public string Type { get; set; }
+
+
+    public void _doSomthing(Store obj)
+    {
+
+        if(obj.Type  == "hypothetical")
+        {
+
+        //do by EF Operation 
+        }else if(obj.Type == "Physical")
+        {
+            //do by EF Operation 
+        }
+        else if(obj.Type == "unscheduled")
+        {
+
+        }
+    }
+}
